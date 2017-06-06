0b3c3978656e27808760b938d15b1c501ca43704
Ryguy-1 <league@WTS10.attlocal.net>
2017-06-06 16:57:40 -0700

    6/6/17

---
+++ src/SmurfsRunner.java
@@ -0,0 +1,18 @@
+/* 
+ * 1. Watch this smurf cartoon: https://www.youtube.com/watch?v=RqbpzEHuO2g
+ * 2. In a Runner class, make a Handy Smurf. Use the methods below to make him eat, and print his name.
+ * 
+ * 5. Make a Papa Smurf and print his name, hat color and girl or boy.
+ * 6. Make a Smurfette and print her name, hat color and girl or boy. */
+
+public class SmurfsRunner {
+public static void main(String[] args) {
+	Smurf Handy = new Smurf("Handy");
+	System.out.println(Handy.getName());
+	Handy.eat();
+	Smurf Papa = new Smurf("Papa");
+	System.out.println(Papa.getName());
+	
+	
+}
+}
