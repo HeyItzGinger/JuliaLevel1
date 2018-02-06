b264a759d6f3e28c47afded91b8936de8482ba66
HeyItzGinger <league@WTS-IM-6.attlocal.net>
2018-02-05 16:57:15 -0800

    NBA Scoreboard (ip)

--- src/NBAController.java
+++ src/NBAController.java
@@ -1,6 +1,45 @@
+import java.awt.BorderLayout;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 public class NBAController {
 public static void main(String[] args) {
 	NBAScoreboard nbaScore = new NBAScoreboard();
+	NBAController npsControl = new NBAController();
+}
+public NBAController() {
+	JFrame frame = new JFrame();
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.setVisible(true);
+	frame.setLayout(new BorderLayout());
+	
+	//Home Setup
+	JPanel home = new JPanel();
+	home.setLayout(new BorderLayout());
+	frame.add(home, BorderLayout.WEST);
+	
+	JPanel home1 = new JPanel();
+	home.add(home1, BorderLayout.NORTH);
+	JLabel Hlabel = new JLabel("Home");
+	home1.add(Hlabel);
+	JPanel home2 = new JPanel();
+	home.add(home2, BorderLayout.CENTER);
+	JLabel HscoreLabel = new JLabel("Score: ");
+	home2.add(HscoreLabel);
+	JButton Hplus = new JButton("+1");
+	home2.add(Hplus);
+	JButton Hminus = new JButton("-1");
+	home2.add(Hminus);
+	JPanel home3 = new JPanel();
+	home.add(home3, BorderLayout.SOUTH);
+	JLabel TOlabel = new JLabel("Timeouts: ");
+	home3.add(TOlabel);
+	JButton TOminus = new JButton("-1");
+	home3.add(TOminus);
+	frame.pack();
+	//Away Setup
+	
 }
 }
--- src/NBAScoreboard.java
+++ src/NBAScoreboard.java
@@ -11,19 +11,24 @@ public class NBAScoreboard {
 	
 	JPanel home = new JPanel();
 	JPanel HomelabelPanel = new JPanel();
+	int HscoreValue = 0;
+	int AscoreValue = 0;
+	int HtimeoutValue = 2;
+	int AtimeoutValue = 2;
 	JPanel homeScore = new JPanel();
 	JPanel homeTOScore = new JPanel();
 	JLabel home1 = new JLabel("Home");
-	JButton home2 = new JButton("Score: ");
-	JButton home3 = new JButton("Timeouts: ");
+	JButton home2 = new JButton("Score: " + HscoreValue);
+	JButton home3 = new JButton("Timeouts: " + HtimeoutValue);
 	
 	JPanel away = new JPanel();
 	JPanel AwaylabelPanel = new JPanel();
 	JLabel away1 = new JLabel("Away");
 	JPanel awayScore = new JPanel();
-	JButton away2 = new JButton("Score: ");
+	JButton away2 = new JButton("Score: " + AscoreValue);
 	JPanel awayTOScore = new JPanel();
-	JButton away3 = new JButton("Timeouts: ");
+	JButton away3 = new JButton("Timeouts: " + AtimeoutValue);
+	
 
 	public NBAScoreboard() {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -48,6 +53,9 @@ public class NBAScoreboard {
 		awayTOScore.add(away3);
 		homeScore.setOpaque(true);
 		homeScore.setBackground(new Color(235, 90, 254));
+		awayScore.setOpaque(true);
+		awayScore.setBackground(new Color(133, 98, 245));
+		frame.pack();
 		
 	}
 }
