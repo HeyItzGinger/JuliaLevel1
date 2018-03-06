9984302682938a26b14b1d821e6cb31f89af253d
HeyItzGinger <league@WTS-IM-6.attlocal.net>
2018-03-05 16:33:03 -0800

    Olympics (ip)

---
+++ src/Olympics.java
@@ -0,0 +1,8 @@
+
+public class Olympics {
+public static void main(String[] args) {
+	oAthlete a1 = new oAthlete("Julia", "Canada", "Volleyball");
+	oAthlete a2 = new oAthlete("Alex", "USA", "Basketball");
+	oAthlete a3 = new oAthlete("Olivia", "Coasterica", "Bobsleding");
+}
+}
---
+++ src/oAthlete.java
@@ -0,0 +1,15 @@
+
+public class oAthlete {
+	String name;
+	String country;
+	String sport;
+	int gold;
+	int silver;
+	int bronze;
+	public oAthlete (String name, String country, String sport) {
+		this.name = name;
+		this.country = country;
+		this.sport = sport;
+	}
+	
+}
