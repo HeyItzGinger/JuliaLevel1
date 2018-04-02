d8c221b8643b45e21d97a790f236bd112133b4a7
ryangleason <league@iMac-2.attlocal.net>
2018-04-02 16:58:11 -0700

    Colored Buttons (ip)

--- src/ColoredButton.java
+++ src/ColoredButton.java
@@ -1,15 +1,30 @@
 import java.awt.Color;
+import java.awt.Dimension;
 import java.util.Random;
+
+import javax.swing.JButton;
 
 public class ColoredButton {
 	Random r = new Random();
-public ColoredButton () {
-	
-}
+	Color c;
  private Color getRandomColor() {
 	int ran = r.nextInt(3);
 	if(ran == 0) {
 		return Color.YELLOW;
 	}
+	else if (ran == 1) {
+		return Color.GREEN;
+	}
+	else {
+		return Color.PINK;		
+	}
 }
+ public JButton getNewButton() {
+	 JButton b = new JButton();
+	 Color c = getRandomColor();
+	 b.setOpaque(true);
+	 b.setBackground(c);
+	 b.setPreferredSize(new Dimension(100,100));
+	 return b;
+ }
 }
--- src/ColoredButtonRunner.java
+++ src/ColoredButtonRunner.java
@@ -1,6 +1,41 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class ColoredButtonRunner {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class ColoredButtonRunner implements ActionListener {
+	JFrame frame = new JFrame();
 	public static void main(String[] args) {
-		
+		new ColoredButtonRunner();
+	}
+	public ColoredButtonRunner() {
+		drawButtons();
+	}
+	public void drawButtons() {
+		frame.setVisible(true);
+		JPanel panel = new JPanel();
+		frame.add(panel);
+		ColoredButton CB = new ColoredButton();
+		JButton b1 = CB.getNewButton();
+		JButton b2 = CB.getNewButton();
+		JButton b3 = CB.getNewButton();
+		JButton b4 = CB.getNewButton();
+		panel.add(b1);
+		panel.add(b2);
+		panel.add(b3);
+		panel.add(b4);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JButton redraw = new JButton("Redraw");
+		panel.add(redraw);
+		redraw.addActionListener(this);
+		frame.pack();
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		frame.dispose();
+		drawButtons();
 	}
 }
