9cc1d11e285afb159e19257916df8af71d139356
HeyItzGinger <league@iMac-2.attlocal.net>
2018-04-16 16:42:54 -0700

    Colored Buttons, Methods WS

--- src/ColoredButton.java
+++ src/ColoredButton.java
@@ -5,17 +5,21 @@ import java.util.Random;
 import javax.swing.JButton;
 
 public class ColoredButton {
-	Random r = new Random();
-	Color c;
+	private Random r = new Random();
+	private Color c;
+	private String col;
  private Color getRandomColor() {
 	int ran = r.nextInt(3);
 	if(ran == 0) {
+		col = "yellow";
 		return Color.YELLOW;
 	}
 	else if (ran == 1) {
+		col = "green";
 		return Color.GREEN;
 	}
 	else {
+		col = "pink";
 		return Color.PINK;		
 	}
 }
@@ -26,5 +30,8 @@ public class ColoredButton {
 	 b.setBackground(c);
 	 b.setPreferredSize(new Dimension(100,100));
 	 return b;
+ }
+ public String getButtonColor() {
+	 return col;
  }
 }
--- src/ColoredButtonRunner.java
+++ src/ColoredButtonRunner.java
@@ -3,10 +3,15 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class ColoredButtonRunner implements ActionListener {
 	JFrame frame = new JFrame();
+	ColoredButton CB;
+	ColoredButton CB1;
+	ColoredButton CB2;
+	ColoredButton CB3;
 	public static void main(String[] args) {
 		new ColoredButtonRunner();
 	}
@@ -17,11 +22,14 @@ public class ColoredButtonRunner implements ActionListener {
 		frame.setVisible(true);
 		JPanel panel = new JPanel();
 		frame.add(panel);
-		ColoredButton CB = new ColoredButton();
+		CB = new ColoredButton();
+		CB1 = new ColoredButton();
+		CB2 = new ColoredButton();
+		CB3 = new ColoredButton();
 		JButton b1 = CB.getNewButton();
-		JButton b2 = CB.getNewButton();
-		JButton b3 = CB.getNewButton();
-		JButton b4 = CB.getNewButton();
+		JButton b2 = CB1.getNewButton();
+		JButton b3 = CB2.getNewButton();
+		JButton b4 = CB3.getNewButton();
 		panel.add(b1);
 		panel.add(b2);
 		panel.add(b3);
@@ -37,5 +45,6 @@ public class ColoredButtonRunner implements ActionListener {
 		// TODO Auto-generated method stub
 		frame.dispose();
 		drawButtons();
+		JOptionPane.showMessageDialog(null, "The button colors are .. " + CB.getButtonColor() + ", " + CB1.getButtonColor() + ", " + CB2.getButtonColor() + ", " + CB3.getButtonColor() + "." );
 	}
 }
